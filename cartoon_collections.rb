b836db913c4dde92499ea8e627e364ea2be58904
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-15 08:29:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(arr)# code an argument here
   # Your code here
-  arr.map { |name| "#{name.capitalize()}!" }
+  arr.map { |name| "#{name.capitalize}!" }
 end
 
 def long_planeteer_calls(arr)# code an argument here
